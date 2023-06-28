edd84a46cbb617ad73ece914b00f288c4735cb70
MateuszBuszmann <mateusz.buszmann@gmail.com>
2023-06-28 19:16:50 +0200

    zadanie1

---
+++ projekt1/Osoba.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+public class Osoba
+{
+    private string imie;
+    private string nazwisko;
+
+    public Osoba(string imieNazwisko)
+    {
+        ImieNazwisko = imieNazwisko;
+    }
+
+    public string Imie
+    {
+        get { return imie; }
+        set
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentException("Imię nie może być puste.");
+            }
+            imie = value;
+        }
+    }
+
+    public string Nazwisko
+    {
+        get { return nazwisko; }
+        set { nazwisko = value; }
+    }
+
+    public DateTime? DataUrodzenia { get; set; }
+    public DateTime? DataSmierci { get; set; }
+
+    public string ImieNazwisko
+    {
+        get { return $"{imie} {nazwisko}".Trim(); }
+        set
+        {
+            var splitted = value.Split(' ');
+            Imie = splitted[0];
+            Nazwisko = splitted.Length > 1 ? splitted[splitted.Length - 1] : string.Empty;
+        }
+    }
+
+    public TimeSpan? Wiek
+    {
+        get
+        {
+            if (DataUrodzenia == null)
+            {
+                return null;
+            }
+            var endDate = DataSmierci ?? DateTime.Now;
+            return endDate - DataUrodzenia.Value;
+        }
+    }
+}
--- projekt1/Program.cs
+++ projekt1/Program.cs
@@ -1,3 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.ReadKey(true);
+﻿Osoba osoba = new Osoba("Jan Kowalski");
+osoba.DataUrodzenia = new DateTime(1999, 3, 6);
+osoba.DataSmierci = new DateTime(2090, 12, 31);
+
+Console.WriteLine($"Imię: {osoba.Imie}");
+Console.WriteLine($"Nazwisko: {osoba.Nazwisko}");
+Console.WriteLine($"Imię i nazwisko: {osoba.ImieNazwisko}");
+Console.WriteLine($"Wiek: {osoba.Wiek}");
+
+// Wyjątek przy próbie ustawienia pustego imienia
+osoba.Imie = "";
+
