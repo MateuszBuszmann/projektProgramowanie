951ea01c96dcf90841bb27761bb40e87a8edca69
MateuszBuszmann <mateusz.buszmann@gmail.com>
2023-06-28 20:54:13 +0200

    Zadanie5

---
+++ projekt1/Macierz.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+public class Macierz<T> : IEquatable<Macierz<T>> where T : IEquatable<T>
+{
+    private T[,] tablica;
+
+    public int WymiarWierszy { get; }
+    public int WymiarKolumn { get; }
+
+    public Macierz(int wymiarWierszy, int wymiarKolumn)
+    {
+        WymiarWierszy = wymiarWierszy;
+        WymiarKolumn = wymiarKolumn;
+
+        tablica = new T[wymiarWierszy, wymiarKolumn];
+    }
+
+    public T this[int wiersz, int kolumna]
+    {
+        get { return tablica[wiersz, kolumna]; }
+        set { tablica[wiersz, kolumna] = value; }
+    }
+
+    public bool Equals(Macierz<T> other)
+    {
+        if (other == null)
+            return false;
+
+        if (WymiarWierszy != other.WymiarWierszy || WymiarKolumn != other.WymiarKolumn)
+            return false;
+
+        for (int i = 0; i < WymiarWierszy; i++)
+        {
+            for (int j = 0; j < WymiarKolumn; j++)
+            {
+                if (!tablica[i, j].Equals(other[i, j]))
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj is Macierz<T>)
+            return Equals((Macierz<T>)obj);
+
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        return tablica.GetHashCode();
+    }
+
+    public static bool operator ==(Macierz<T> m1, Macierz<T> m2)
+    {
+        if (ReferenceEquals(m1, m2))
+            return true;
+
+        if (m1 is null || m2 is null)
+            return false;
+
+        return m1.Equals(m2);
+    }
+
+    public static bool operator !=(Macierz<T> m1, Macierz<T> m2)
+    {
+        return !(m1 == m2);
+    }
+}
--- projekt1/Program.cs
+++ projekt1/Program.cs
@@ -3,9 +3,11 @@ public class Program
 {
     private static void Main(string[] args)
     {
-        //Zadanie1();
-        //Zadanie2();
+        Zadanie1();
+        Zadanie2();
         Zadanie3();
+        Zadanie4();
+
 
     }
     private static void Zadanie1()
@@ -19,10 +21,10 @@ public class Program
         Console.WriteLine($"Imię i nazwisko: {osoba.ImieNazwisko}");
         Console.WriteLine($"Wiek: {osoba.Wiek}");
 
-        // Wyjątek przy próbie ustawienia pustego imienia
-        osoba.Imie = "";
+        
     }
-    private static void Zadanie2() {
+    private static void Zadanie2()
+    {
         Prostokat prostokat1 = new Prostokat(15, 30);
         Console.WriteLine($"Bok A: {prostokat1.BokA}");
         Console.WriteLine($"Bok B: {prostokat1.BokB}");
@@ -31,7 +33,8 @@ public class Program
         Console.WriteLine($"Bok A: {prostokat2.BokA}");
         Console.WriteLine($"Bok B: {prostokat2.BokB}");
     }
-    private static void Zadanie3() {
+    private static void Zadanie3()
+    {
         Wektor v1 = new Wektor(1, 2, 3);
         Wektor v2 = new Wektor(4, 5, 6);
         Wektor v3 = new Wektor(1, 1, 1);
@@ -51,21 +54,47 @@ public class Program
         }
 
         Wektor suma = Wektor.Suma(v1, v2, v3);
-        Console.WriteLine($"Suma v1, v2 i v3: {string.Join(", ", suma)}");
+        Console.WriteLine($"Suma v1, v2 i v3: {suma}");
 
         Wektor v4 = v1 + v2;
-        Console.WriteLine($"v4 = v1 + v2: {string.Join(", ", v4)}");
+        Console.WriteLine($"v4 = v1 + v2: {v4}");
 
         Wektor v5 = v1 - v2;
-        Console.WriteLine($"v5 = v1 - v2: {string.Join(", ", v5)}");
+        Console.WriteLine($"v5 = v1 - v2: {v5}");
 
         Wektor v6 = v1 * 2.5;
-        Console.WriteLine($"v6 = v1 * 2.5: {string.Join(", ", v6)}");
+        Console.WriteLine($"v6 = v1 * 2.5: {v6}");
 
         Wektor v7 = 3.5 * v2;
-        Console.WriteLine($"v7 = 3.5 * v2: {string.Join(", ", v7)}");
-
+        Console.WriteLine($"v7 = 3.5 * v2: {v7}");
+            
         Wektor v8 = v2 / 2;
-        Console.WriteLine($"v8 = v2 / 2: {string.Join(", ", v8)}");
+        Console.WriteLine($"v8 = v2 / 2: {v8}");
+    }   
+    private static void Zadanie4()
+    {
+        Macierz<int> m1 = new Macierz<int>(2, 2);
+        m1[0, 0] = 1;
+        m1[0, 1] = 2;
+        m1[1, 0] = 3;
+        m1[1, 1] = 4;
+
+        Macierz<int> m2 = new Macierz<int>(2, 2);
+        m2[0, 0] = 1;
+        m2[0, 1] = 2;
+        m2[1, 0] = 3;
+        m2[1, 1] = 4;
+
+        Macierz<int> m3 = new Macierz<int>(2, 2);
+        m3[0, 0] = 5;
+        m3[0, 1] = 6;
+        m3[1, 0] = 7;
+        m3[1, 1] = 8;
+
+        Console.WriteLine($"m1 == m2: {m1 == m2}");
+        Console.WriteLine($"m1 != m2: {m1 != m2}");
+        Console.WriteLine($"m1 == m3: {m1 == m3}");
+        Console.WriteLine($"m1 != m3: {m1 != m3}");
+
     }
 }
