819f0f3bb8206024fdb26e793db722916958ef5e
MateuszBuszmann <mateusz.buszmann@gmail.com>
2023-06-28 20:43:01 +0200

    Zadanie3

--- projekt1/Program.cs
+++ projekt1/Program.cs
@@ -4,7 +4,8 @@ public class Program
     private static void Main(string[] args)
     {
         //Zadanie1();
-        Zadanie2();
+        //Zadanie2();
+        Zadanie3();
 
     }
     private static void Zadanie1()
@@ -29,5 +30,42 @@ public class Program
         Prostokat prostokat2 = Prostokat.ArkuszPapieru("A0");
         Console.WriteLine($"Bok A: {prostokat2.BokA}");
         Console.WriteLine($"Bok B: {prostokat2.BokB}");
+    }
+    private static void Zadanie3() {
+        Wektor v1 = new Wektor(1, 2, 3);
+        Wektor v2 = new Wektor(4, 5, 6);
+        Wektor v3 = new Wektor(1, 1, 1);
+
+        Console.WriteLine($"Wymiar v1: {v1.Wymiar}");
+        Console.WriteLine($"Długość v1: {v1.Dlugosc}");
+        Console.WriteLine($"Współrzędna v1[0]: {v1[0]}");
+
+        double? iloczynSkalarny = Wektor.IloczynSkalarny(v1, v2);
+        if (iloczynSkalarny.HasValue)
+        {
+            Console.WriteLine($"Iloczyn skalarny v1 i v2: {iloczynSkalarny.Value}");
+        }
+        else
+        {
+            Console.WriteLine("Wektory mają różne wymiary.");
+        }
+
+        Wektor suma = Wektor.Suma(v1, v2, v3);
+        Console.WriteLine($"Suma v1, v2 i v3: {string.Join(", ", suma)}");
+
+        Wektor v4 = v1 + v2;
+        Console.WriteLine($"v4 = v1 + v2: {string.Join(", ", v4)}");
+
+        Wektor v5 = v1 - v2;
+        Console.WriteLine($"v5 = v1 - v2: {string.Join(", ", v5)}");
+
+        Wektor v6 = v1 * 2.5;
+        Console.WriteLine($"v6 = v1 * 2.5: {string.Join(", ", v6)}");
+
+        Wektor v7 = 3.5 * v2;
+        Console.WriteLine($"v7 = 3.5 * v2: {string.Join(", ", v7)}");
+
+        Wektor v8 = v2 / 2;
+        Console.WriteLine($"v8 = v2 / 2: {string.Join(", ", v8)}");
     }
 }
---
+++ projekt1/Wektor.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Xml.Linq;
+
+public class Wektor
+{
+    private double[] wspolrzedne;
+
+    public Wektor(byte wymiar)
+    {
+        wspolrzedne = new double[wymiar];
+    }
+
+    public Wektor(params double[] wspolrzedne)
+    {
+        this.wspolrzedne = wspolrzedne;
+    }
+
+    public double Dlugosc
+    {
+        get { return (double)IloczynSkalarny(this, this); }
+    }
+
+    public byte Wymiar
+    {
+        get { return (byte)wspolrzedne.Length; }
+    }
+
+    public double this[byte index]
+    {
+        get { return wspolrzedne[index]; }
+    }
+
+    public static double? IloczynSkalarny(Wektor V, Wektor W)
+    {
+        if (V.Wymiar != W.Wymiar)
+            return null;
+
+        double iloczyn = 0;
+        for (int i = 0; i < V.Wymiar; i++)
+        {
+            iloczyn += V.wspolrzedne[i] * W.wspolrzedne[i];
+        }
+
+        return iloczyn;
+    }
+
+    public static Wektor Suma(params Wektor[] wektory)
+    {
+        if (wektory.Length == 0)
+        {
+            throw new ArgumentException("Brak wektorów do zsumowania.");
+        }
+
+        byte wymiar = wektory[0].Wymiar;
+        for (int i = 1; i < wektory.Length; i++)
+        {
+            if (wektory[i].Wymiar != wymiar)
+            {
+                throw new ArgumentException("Wektory mają różne wymiary.");
+            }
+        }
+
+        Wektor suma = new Wektor(wymiar);
+        for (int i = 0; i < wymiar; i++)
+        {
+            for (int j = 0; j < wektory.Length; j++)
+            {
+                suma.wspolrzedne[i] += wektory[j].wspolrzedne[i];
+            }
+        }
+
+        return suma;
+    }
+
+    public static Wektor operator +(Wektor v1, Wektor v2)
+    {
+        return Suma(v1, v2);
+    }
+
+    public static Wektor operator -(Wektor v1, Wektor v2)
+    {
+        Wektor negatedV2 = new Wektor((byte)v2.wspolrzedne.Length);
+        for (int i = 0; i < v2.wspolrzedne.Length; i++)
+        {
+            negatedV2.wspolrzedne[i] = -v2.wspolrzedne[i];
+
+        }
+
+        return Suma(v1, negatedV2);
+    }
+
+    public static Wektor operator *(Wektor v, double scalar)
+    {
+        double[] multiplied = new double[v.wspolrzedne.Length];
+        for (int i = 0; i < v.wspolrzedne.Length; i++)
+        {
+            multiplied[i] = v.wspolrzedne[i] * scalar;
+        }
+
+        return new Wektor(multiplied);
+    }
+
+    public static Wektor operator *(double scalar, Wektor v)
+    {
+        return v * scalar;
+    }
+
+    public static Wektor operator /(Wektor v, double scalar)
+    {
+        if (v is null)
+        {
+            throw new ArgumentNullException(nameof(v));
+        }
+
+        if (scalar == 0)
+        {
+            throw new ArgumentException("Dzielenie przez zero.");
+        }
+        double[] divided = new double[v.wspolrzedne.Length];
+        for (int i = 0; i < v.wspolrzedne.Length; i++)
+        {
+            divided[i] = v.wspolrzedne[i] / scalar;
+        }
+
+        return new Wektor(divided);
+    }
+    public override string ToString()
+    {
+        string result = "Wektor:";
+        for (int i = 0; i < wspolrzedne.Length; i++)
+        {
+            result += $" {wspolrzedne[i]}";
+        }
+        return result;
+    }
+}
+
+
